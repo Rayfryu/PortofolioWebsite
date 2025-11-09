b972f4269868ea902533e61d99e664e7914b26f0
Rayfryu <rayhanallayyubi2021@gmail.com>
2025-11-09 15:52:49 +0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,108 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Bisnis Anda layak mendapatkan fondasi digital yang cepat, aman, dan siap masa depan. Saya membangun web app kustom dengan arsitektur bersih, database teroptimasi, dan integrasi mulus—menjamin keandalan di setiap lapisannya.",
+    items: [
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Microservices, Auth Systems)",
+      },
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+  title: "Cyber Security & CTF Solutions",
+  description:
+    "Keamanan digital bukan sekadar opsi—itu keharusan. Saya membangun sistem yang tahan eksploitasi, melakukan analisis kerentanan, dan mengembangkan lingkungan CTF interaktif untuk melatih keterampilan ofensif dan defensif secara nyata.",
+  items: [
+    {
+      title: "Vulnerability Assessment",
+      description: "(Reconnaissance, Exploitation, Privilege Escalation)",
+    },
+    {
+      title: "CTF Environment Setup",
+      description: "(Web, Forensic, Crypto, Reverse Engineering Challenges)",
+    },
+    {
+      title: "Security Automation",
+      description: "(Monitoring, Log Analysis, Intrusion Detection)",
+    },
+  ],
+}
+];
+export const projects = [
+  {
+    id: 1,
+    name: "Juara Harapan Dua Website Developer Tingkat Nasional",
+    description:
+      "An online store specializing in phone accessories including cases, chargers, cables, and power banks with MagSafe compatibility.",
+    href: "",
+    image: "/assets/projects/satu.jpg",
+    bgImage: "/assets/backgrounds/satu.jpg",
+    frameworks: [
+      { id: 1, name: "12-13 Juni 2025" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Penghargaan Lomba Website Design Sagasitas Indonesia",
+    description:
+      "An online store specializing in rare and decorative plants with a clean, user-friendly interface.",
+    href: "",
+    image: "/assets/projects/dua.jpg",
+    bgImage: "/assets/backgrounds/dua.jpg",
+    frameworks: [
+      { id: 1, name: "26-30 Agustus 2024" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Peserta IT Camp Android Apps",
+    description:
+      "An e-commerce platform for Apple products and accessories with deals and category filtering.",
+    href: "",
+    image: "/assets/projects/tiga.jpg",
+    bgImage: "/assets/backgrounds/tiga.jpg",
+    frameworks: [
+      { id: 1, name: "1 Maret 2022" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Penghargaan Website Design IT Competition",
+    description:
+      "A multi-category online shop featuring electronics, home appliances, and gaming gear with special offers.",
+    href: "",
+    image: "/assets/projects/empat.jpg",
+    bgImage: "/assets/backgrounds/empat.jpg",
+    frameworks: [
+      { id: 1, name: "14 Februari 2025" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Workshop Laravel Breeze Dicoding Indonesia",
+    description:
+      "A curated collection of designer home decor items, including furniture and artisan vases.",
+    href: "",
+    image: "/assets/projects/lima.jpg",
+    bgImage: "/assets/backgrounds/lima.jpg",
+    frameworks: [
+      { id: 1, name: "24-25 Januari 2025" },
+    ],
+  },
+];
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/fllost.0/" },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/abdullah-rayhan-al-ayyubi-9849472a8/" },
+  { name: "GitHub", href: "https://github.com/Rayfryu" },
+];
